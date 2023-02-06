2e8241f0fb8c38403ba27ba14f2397daaacd88cf
pro100Kevin <gureev.i.vl@yandex.ru>
2023-02-06 23:30:04 +0300

    3.4 Вывод всей таблицы умножения на один

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("pro100Kevin");
+        System.out.println("01.10.1990");
+    }
+}
