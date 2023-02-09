a5316d57c5c50cf6377e542cac789e8f57fcf911
pro100Kevin <gureev.i.vl@yandex.ru>
2023-02-09 23:07:54 +0300

    Калькулятор примитивных переменных

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,10 +2,14 @@ package ru.job4j.calculator;
 
 public class Calculator {
     public static void main(String[] args) {
-        int size = 10;
-        size = 100;
-        size = size - 5;
-        boolean result;
-        System.out.println(result);
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six / 2;
+        int fiveMinusTwo = five - 2;
+        int fourTimeTwo = four * 2;
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
     }
 }
