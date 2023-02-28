60fe182b80f1e32c8298fa4416654420f789ee4a
pro100Kevin <gureev.i.vl@yandex.ru>
2023-02-28 07:41:00 +0300

    Работа с методами и добавленем аргументов

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -6,9 +6,7 @@ public class ArgMethod {
     }
 
     public static void main(String[]args) {
-
         String name = "Job4j";
-        
         ArgMethod.hello(name);
         ArgMethod.hello(name);
         ArgMethod.hello(name);
