08611d14fa0cbc58e41f3ee7313ce31f6c70297e
pro100Kevin <gureev.i.vl@yandex.ru>
2023-03-14 23:53:09 +0300

    конвертер валют

---
+++ src/main/java/ru/job4j/ru/job4j/converter/Converter.java
@@ -0,0 +1,35 @@
+package ru.job4j.ru.job4j.converter;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return rsl;
+    }
+
+    public static float rubleToDollar(float value) {
+        float rsl = value / 73.5f;
+        return rsl;
+    }
+
+    public static float euroToDollar(float value) {
+        float rsl = value * 1.17f;
+        return rsl;
+
+    }
+
+    public static float dollarToEuro(float value) {
+        float rsl = value * 0.85f;
+        return rsl;
+    }
+
+    public static void main(String[]  args) {
+        float euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles is " + euro + " euro.");
+        euro = Converter.dollarToEuro(17);
+        System.out.println("17 dollars is " + euro + " euro.");
+        float dollars = Converter.rubleToDollar(567);
+        System.out.println("567 rubles is " + dollars + " dollars.");
+        dollars = Converter.euroToDollar(18);
+        System.out.println("18 euro is " + dollars + " dollars.");
+    }
+}
