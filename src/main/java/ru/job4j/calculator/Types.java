49acf06f1863a3215f38a24ee3b19062d7d7e38f
pro100Kevin <gureev.i.vl@yandex.ru>
2023-05-25 22:08:34 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/ru/job4j/Info.java

---
+++ src/main/java/ru/job4j/calculator/Types.java
@@ -0,0 +1,29 @@
+package ru.job4j.calculator;
+
+public class Types {
+    public static void main(String[] args) {
+        long l = 129;
+        byte b = (byte) l;
+        System.out.println(l);
+        System.out.println(b);
+        float f = 12358.7f;
+        int i = 45981;
+        short rsl = (short) (f + i);
+        System.out.println(f);
+        System.out.println(i);
+        System.out.println(rsl);
+        char c = 45000;
+        float ii = c;
+        System.out.println(c);
+        System.out.println(ii);
+        double d = 121.19;
+        byte bb = (byte) d;
+        System.out.println(d);
+        System.out.println(bb);
+        short s = 1500;
+        char cc = (char) s;
+        double dd = cc;
+        System.out.println(cc);
+        System.out.println(dd);
+    }
+}
