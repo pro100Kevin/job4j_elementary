a06a3e14ddd4d2e032cc89dbda34a6d5833343ed
pro100Kevin <gureev.i.vl@yandex.ru>
2023-05-25 21:28:58 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/ru/job4j/Info.java

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -9,9 +9,9 @@ public class SqArea {
     }
 
     public static void main (String[] args) {
-            int result1 = square(4, 1);
-            System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
-            int result2 = square(6, 2);
-            System.out.println(" p = 6, k = 2, s = 2, real = " + result2);
+        int result1 = square(4, 1);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+        int result2 = square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result2);
         }
     }
