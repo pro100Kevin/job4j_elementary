ac8daf4420b3cd59b34c76f1e35745a8f67c1aac
Kevin <gureev.i.vl@yandex.ru>
2024-11-18 15:40:03 +0300

    task

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -4,11 +4,12 @@ public class SqArea {
     public static int square(int p, int k) {
         int h = p / (2 * k + 2);
         int l = h * k;
-        int square = l * h;
+        int square;
+        square = l * h;
         return square;
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int result1 = square(4, 1);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
         int result2 = square(6, 2);
